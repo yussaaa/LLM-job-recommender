d8f48c4405d5c252dd33b30e712bebe120eb87ee
yussaaa <yusa.li@mail.utoronto.ca>
2024-01-09 20:41:41 -0500

    initialize chalice

---
+++ app.py
@@ -0,0 +1,35 @@
+from chalice import Chalice
+from src.main import main
+import json
+
+app = Chalice(app_name="job_rec_app")
+
+
+@app.route("/")
+def index():
+    jobs = main()
+    if not jobs:
+        return {"400": "No jobs found"}
+
+    return {"200": "Success!"}
+
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
