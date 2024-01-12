e941651810409debde34480a3db9dcba3305e445
yussaaa <yusa.li@mail.utoronto.ca>
2024-01-11 22:33:27 -0500

    update package path for setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="LLM_aws_job_rec",
     version="0.0.1",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(where="chalicelib"),
+    package_dir={"": "llm_job_app/chalicelib"},
     long_description=open("README.md").read(),
     install_requires=[],
 )
