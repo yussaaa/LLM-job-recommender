58ed6b885e0b0c8cb7853f8b6ccdfcda6f52c0fe
yussaaa <yusa.li@mail.utoronto.ca>
2024-01-12 01:31:59 -0500

    sync the package name to chalicelib

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="LLM_aws_job_rec",
+    name="chalicelib",
     version="0.0.1",
     packages=find_packages(where="chalicelib"),
-    package_dir={"": "llm_job_app/chalicelib"},
+    # package_dir={"chalicelib": "./llm_job_app/chalicelib"},
     long_description=open("README.md").read(),
     install_requires=[],
 )
