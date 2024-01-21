ad0c0328cf3e2c62bf05f9ea90fe5b2f5b993680
yussaaa <yusa.li@mail.utoronto.ca>
2024-01-20 22:26:43 -0500

    add index error handling

--- llm_job_app/chalicelib/aws/opensearch_class.py
+++ llm_job_app/chalicelib/aws/opensearch_class.py
@@ -35,11 +35,18 @@ class OpenSearch_custom:
             dict_data (dict): data to be saved
             index_name (str): index name
         """
-        # Save the dictionary to OpenSearch
-        response = self.client.index(index=index_name, body=dict_data)
-
-        # Print the response
-        print(response)
+        try:
+            # Save the dictionary to OpenSearch
+            response = self.client.index(
+                index=index_name, body=dict_data, pipeline="add-timestamp"
+            )
+            # Print the response
+            print(response)
+        except Exception as e:
+            print(e)
+            print("=========Error saving to index for the following data:=========\n")
+            print(dict_data)
+            print()
 
     def create_index(self, index_name: str):
         # Create an index with non-default settings.
@@ -58,7 +65,11 @@ class OpenSearch_custom:
             if self.client.indices.exists(index=index_name):
                 print(f"Index {index_name} already exists")
                 return
-            response = self.client.indices.create(index_name, body=index_body)
+            response = self.client.indices.create(
+                index_name,
+                body=index_body,
+                # pipeline="add-timestamp",
+            )
             print("\nCreating index:")
             print(response)
         except Exception as e:
@@ -69,46 +80,45 @@ if __name__ == "__main__":
     host = "search-ui-test-j4slt3fz7uu5ahsm6vgu3hwqem.us-east-2.es.amazonaws.com"
     os_client = OpenSearch_custom(host=host)
     index_name = "parsed-jobs-index"
-    os_client.create_index(index_name=index_name)
+    # os_client.create_index(index_name=index_name)
     # job_parsed = {\n  "job_basics": {\n    "title": "Sr Data Engineer",\n    "company": "Royal Bank of Canada",\n    "industry": "Banking/Financial Services",\n    "location": "Toronto, ON, Canada",\n    "years_of_experience_required": "5+ years",\n    "employment_type": "Full-time",\n    "source": "Jobs At RBC",\n    "application_link": "https://jobs.rbc.com/ca/en/job/R-0000076286/Sr-Data-Engineer?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic"\n  },\n  "required_skills": {\n    "programming_languages": ["Python", "SQL"],\n    "data_visualization_tools": [],\n    "databases": ["NoSQL", "SQL databases"],\n    "web_development": {\n      "front_end_technologies": [],\n      "back_end_technologies": []\n    },\n    "devops_tools": [],\n    "statistical_analysis_methods": [],\n    "machine_learning_frameworks": [],\n    "machine_learning_operations": [],\n    "cloud_platforms": ["Azure", "Snowflake"],\n    "big_data_technologies": ["Hadoop", "PySpark"],\n    "natural_language_processing": [],\n    "deep_learning_concepts": [],\n    "data_engineering_tools": ["ETL/ELT workflows"],\n    "large_models": []\n  },\n  "company_culture": [\n    "diversity and inclusion",\n    "equitable workplace",\n    "coaching and mentoring"\n  ],\n  "job_benefits": {\n    "salary_range": "",\n    "salary_currency": "CAD",\n    "paid_time_off": "",\n    "retirement_plan_matching": false,\n    "professional_development_budget": "",\n    "employee_discounts": "",\n    "remote_work_policy": {\n      "fully_remote_option": false,\n      "partial_remote_option": "",\n      "work_from_anywhere_program": ""\n    },\n    "relocation_support": ""\n  }\n}\n```'
     job_parsed = {
-        "job_basics": {
-            "title": "Sr Data Engineer",
+        "job": {
+            "title": "Principal Engineer - AI & Machine Learning",
             "company": "Royal Bank of Canada",
-            "industry": "Banking/Financial Services",
+            "industry": "Wealth Management",
             "location": "Toronto, ON, Canada",
-            "years_of_experience_required": "5+ years",
-            "employment_type": "Full-time",
-            "source": "Jobs At RBC",
-            "application_link": "https://jobs.rbc.com/ca/en/job/R-0000076286/Sr-Data-Engineer?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+            "years_of_experience_required": "",
+            "employment_type": "Full time",
+            "source": "RBC Careers",
+            "application_link": "https://jobs.rbc.com/ca/en/job/R-0000075808/Principal-Engineer-AI-Machine-Learning?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
         },
-        "required_skills": {
-            "programming_languages": ["Python", "SQL"],
+        "skills": {
+            "programming_languages": ["Python", "Java"],
             "data_visualization_tools": [],
-            "databases": ["NoSQL", "SQL databases"],
+            "databases": [],
             "web_development": {
                 "front_end_technologies": [],
                 "back_end_technologies": [],
             },
-            "devops_tools": [],
+            "devops_tools": ["Docker", "Kubernetes", "OpenShift"],
             "statistical_analysis_methods": [],
             "machine_learning_frameworks": [],
-            "machine_learning_operations": [],
-            "cloud_platforms": ["Azure", "Snowflake"],
-            "big_data_technologies": ["Hadoop", "PySpark"],
+            "machine_learning_operations": ["MLOps"],
+            "cloud_platforms": [],
+            "big_data_technologies": ["Apache Spark", "Hadoop"],
             "natural_language_processing": [],
             "deep_learning_concepts": [],
-            "data_engineering_tools": ["ETL/ELT workflows"],
+            "data_engineering_tools": [],
             "large_models": [],
         },
         "company_culture": [
-            "diversity and inclusion",
-            "equitable workplace",
-            "coaching and mentoring",
+            "Flexible work/life balance options",
+            "Leaders who support your development through coaching and managing opportunities",
         ],
         "job_benefits": {
             "salary_range": "",
-            "salary_currency": "CAD",
+            "salary_currency": "",
             "paid_time_off": "",
             "retirement_plan_matching": False,
             "professional_development_budget": "",
@@ -116,9 +126,10 @@ if __name__ == "__main__":
             "remote_work_policy": {
                 "fully_remote_option": False,
                 "partial_remote_option": "",
-                "work_from_anywhere_program": "",
+                "work_from_anywhere_program": "Up to 90 days/year",
             },
-            "relocation_support": "",
+            "relocation_support": False,
         },
     }
+
     os_client.save_to_index(job_parsed, index_name=index_name)
