f02f0afe2946fd2ee6adfb62838a6208a7268147
yussaaa <yusa.li@mail.utoronto.ca>
2024-01-24 21:37:04 -0500

    update pipeline with embedding step

--- llm_job_app/chalicelib/main.py
+++ llm_job_app/chalicelib/main.py
@@ -5,6 +5,8 @@ from apify.scrapped_formatting import filter_full_job_info
 from aws.opensearch_class import OpenSearch_custom
 
 from llm_chain.custom_chain import JobParserLLM, json_parsed_sample
+from llm_chain.embedding import EmbeddingModel
+from llm_chain.embedding_feature_prepare_job import filter_skills_and_job
 
 import os
 from dotenv import load_dotenv
@@ -14,11 +16,11 @@ import time
 import sys
 
 # # Add a logging handler that directs logs to the terminal
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+# logging.basicConfig(stream=sys.stdout, level=print)
 # # Set up logging
 # logging.basicConfig(
 #     filename="main.log",
-#     level=logging.INFO,
+#     level=print,
 #     filemode="w",
 #     format="%(asctime)s - %(levelname)s - %(message)s",
 # )
@@ -26,13 +28,13 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 load_dotenv()
 
 
-default_index_name = "llm-jobs-index"
-index_name = "parsed-jobs-index-new"  # "parsed-jobs-index-w-date"
+index_name = "jobs-index"  # "llm-jobs-index"
 
 host = "search-ui-test-j4slt3fz7uu5ahsm6vgu3hwqem.us-east-2.es.amazonaws.com"
 
 os_client = OpenSearch_custom(host=host)
 chat = JobParserLLM(model="gpt-3.5-turbo-1106")
+embedder = EmbeddingModel(model="text-embedding-ada-002")
 
 
 def apify_get_clean(query_URL):
@@ -41,8 +43,11 @@ def apify_get_clean(query_URL):
     start_time = time.time()  # Start time
     total_pages = len(jobs)  # Total number of pages
 
+    print(f"Total number of pages to be processed: {total_pages}")
+
     for page_num, page in enumerate(jobs):
         jobs_info = page["googleJobs"]
+
         for job in jobs_info:
             # Clean the data
             # job = filter_full_job_info(job)
@@ -50,6 +55,11 @@ def apify_get_clean(query_URL):
             try:
                 # Parse the job description
                 job_parsed = chat.parse(job, json_parsed_sample=json_parsed_sample)
+                job_info_to_embedd = filter_skills_and_job(job_parsed)
+
+                # Embed the job description
+                embedding = embedder.get_embedding(job_info_to_embedd)
+                job_parsed["embedding"] = embedding
 
                 # Save the job to OpenSearch
                 os_client.save_to_index(job_parsed, index_name=index_name)
@@ -59,14 +69,14 @@ def apify_get_clean(query_URL):
 
             batch_jobs.append(job_parsed)
 
-        logging.info(f"{page_num} / {total_pages} finished.")
+        print(f"{page_num} / {total_pages} finished.")
 
     end_time = time.time()  # End time
     elapsed_time = end_time - start_time  # Time spent
     num_jobs = len(batch_jobs)  # Number of jobs scraped
 
-    logging.info(f"Time spent: {elapsed_time:.0f} seconds")
-    logging.info(f"Number of jobs scraped: {num_jobs}")
+    print(f"Time spent: {elapsed_time:.0f} seconds")
+    print(f"Number of jobs scraped: {num_jobs}")
 
     return batch_jobs
 
